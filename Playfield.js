599f6ef14d09fa81bd7fb444ee42ab4f88ef26b6
Michael Goodnow <mmgoodnow@gmail.com>
2020-12-01 23:37:06 -0500

    Add rotation data

---
+++ Playfield.js
@@ -0,0 +1,57 @@
+import { ALL_COLORS, PLAYFIELD_HEIGHT, SQUARE_SIZE } from "./constants.js";
+
+export class Playfield {
+	constructor() {
+		const columns = 10;
+		const rows = 40;
+		this.context = document.getElementById("playfield").getContext("2d");
+		this.data = Array.from(new Array(columns), () => new Array(rows));
+		for (let i = 0; i < 10; i++) {
+			for (let j = 0; j < 10; j++) {
+				this.data[i][j] =
+					ALL_COLORS[Math.floor(Math.random() * ALL_COLORS.length)];
+			}
+		}
+	}
+
+	get(x, y) {
+		return this.data[x][y];
+	}
+
+	drawBlockAtCoordinates(x, y, blockColor) {
+		const xCoordinate = SQUARE_SIZE * x;
+		const yCoordinate = PLAYFIELD_HEIGHT - SQUARE_SIZE * y - SQUARE_SIZE;
+		if (blockColor) {
+			this.context.fillStyle = blockColor;
+			this.context.fillRect(
+				xCoordinate + 2,
+				yCoordinate + 2,
+				SQUARE_SIZE - 4,
+				SQUARE_SIZE - 4
+			);
+		} else {
+			this.context.clearRect(
+				xCoordinate,
+				yCoordinate,
+				SQUARE_SIZE,
+				SQUARE_SIZE
+			);
+		}
+		this.context.strokeStyle = blockColor ? `${blockColor}80` : "gray";
+		this.context.strokeRect(
+			xCoordinate + 1,
+			yCoordinate + 1,
+			SQUARE_SIZE - 2,
+			SQUARE_SIZE - 2
+		);
+	}
+
+	draw(fallingPiece) {
+		for (let i = 0; i < 10; i++) {
+			for (let j = 0; j < 20; j++) {
+				const blockColor = this.get(i, j);
+				this.drawBlockAtCoordinates(i, j, blockColor);
+			}
+		}
+	}
+}
---
+++ constants.js
@@ -0,0 +1,21 @@
+export const SQUARE_SIZE = 100;
+export const PLAYFIELD_WIDTH = 1000;
+export const PLAYFIELD_HEIGHT = 2000;
+
+export const GREEN = "#008000";
+export const RED = "#ff0000";
+export const BLUE = "#0000ff";
+export const LIGHT_BLUE = "#00ffff";
+export const PURPLE = "#800080";
+export const ORANGE = "#ffa500";
+export const YELLOW = "#ffff00";
+export const ALL_COLORS = [
+	GREEN,
+	RED,
+	BLUE,
+	LIGHT_BLUE,
+	PURPLE,
+	ORANGE,
+	YELLOW,
+	null,
+];
